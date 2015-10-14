6418226d3ad279fd411cf5ecf9086367c747c905
andrew <andriytheprogrammer@gmail.com>
2015-10-14 18:46:14 +0300

    Repaired production for Heroku. Yes again...

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Book do
-
+  permit_params :title, :author, :description, :book_text
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
